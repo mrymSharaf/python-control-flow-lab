8ced9c791d55cc9e3994b204718b4e912670a182
Mariam Sharaf <maryamshaider25@gmail.com>
2025-08-25 14:46:26 +0300

    fixes

--- exercises.py
+++ exercises.py
@@ -139,13 +139,15 @@ weather_advice()
 # - Check for multiples of BOTH 3 and 5 first to avoid missing them.
 #
 def fizz_buzz():
-    for i in range(1,50):
+    for i in range(1,51):
         if i %3 == 0 and not i %5 == 0:
             print('fizz')
         elif i %5 == 0 and not i %3 == 0:
             print('buzz')
         elif i %3 == 0 and i %5 == 0:
             print('fizz-buzz')
+        else:
+            print(i)
 
 # Call the function
 fizz_buzz()
